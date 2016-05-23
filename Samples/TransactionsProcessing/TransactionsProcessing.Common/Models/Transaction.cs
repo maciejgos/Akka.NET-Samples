39ed8c1672f39cc4fc84bfc5c633d64f9244ab90
Maciej Gos <maciej@maciejgos.com>
2016-05-23 21:40:20 +0200

    Change properties signature
    Change signature of TransactionID & OrderID

--- Samples/TransactionsProcessing/TransactionsProcessing.Common/Models/Transaction.cs
+++ Samples/TransactionsProcessing/TransactionsProcessing.Common/Models/Transaction.cs
@@ -4,10 +4,10 @@ namespace TransactionsProcessing.Common.Models
 {
     public class Transaction : ITransaction
     {
-        public string TransactionID { get; set; }
+        public int TransactionID { get; set; }
         public DateTime TransactionDate { get; set; } //YYYYmmdd
         public decimal Amount { get; set; } //-254.95
         public string Currency { get; set; } //PLN
-        public string OrderID { get; set; }
+        public int OrderID { get; set; }
     }
 }
