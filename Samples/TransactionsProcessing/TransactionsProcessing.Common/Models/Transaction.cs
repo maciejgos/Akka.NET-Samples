4fbffa627791decc84126c72db6996fa52b7ed32
Maciej Gos <maciej@maciejgos.com>
2016-05-23 21:37:57 +0200

    Create transaction model

--- Samples/TransactionsProcessing/TransactionsProcessing.Common/Models/Transaction.cs
+++ Samples/TransactionsProcessing/TransactionsProcessing.Common/Models/Transaction.cs
@@ -1,7 +1,13 @@
-﻿
+﻿using System;
+
 namespace TransactionsProcessing.Common.Models
 {
     public class Transaction : ITransaction
     {
+        public string TransactionID { get; set; }
+        public DateTime TransactionDate { get; set; } //YYYYmmdd
+        public decimal Amount { get; set; } //-254.95
+        public string Currency { get; set; } //PLN
+        public string OrderID { get; set; }
     }
 }
